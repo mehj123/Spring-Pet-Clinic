f56cfa2b57c47f8ae0f9b603d5521153e5846e6e
mehj123 <mehjabeen.begum@outlook.com>
2019-05-28 12:17:12 +0400

    Map Implementation to manage setting of ID property. Closes #24

--- spring-pet-clinic-web/src/main/java/com/learningspring5/springpetclinic/bootstrap/DataLoader.java
+++ spring-pet-clinic-web/src/main/java/com/learningspring5/springpetclinic/bootstrap/DataLoader.java
@@ -22,14 +22,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Anas");
         owner1.setLastName("PM");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Mehjabeen");
         owner2.setLastName("Begum");
 
@@ -38,14 +36,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded owners data...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Fathima");
         vet1.setLastName("PA");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Noora");
         vet2.setLastName("PA");
 
