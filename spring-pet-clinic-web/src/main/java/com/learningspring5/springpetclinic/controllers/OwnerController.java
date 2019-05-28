12534dba5c58e0e571493a437a215b1f41c04039
mehj123 <mehjabeen.begum@outlook.com>
2019-05-28 11:26:49 +0400

    Adding changes for listing owners in index page Services. Closes #16

--- spring-pet-clinic-web/src/main/java/com/learningspring5/springpetclinic/controllers/OwnerController.java
+++ spring-pet-clinic-web/src/main/java/com/learningspring5/springpetclinic/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package com.learningspring5.springpetclinic.controllers;
 
+import com.learningspring5.springpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
-    @RequestMapping({"/","/index","/index.html"})
-    public String getOwners(){
+    private OwnerService ownerService;
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+
+
+    @RequestMapping({"","/","/index","/index.html"})
+    public String getOwners(Model model){
+        model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
     }
 }
