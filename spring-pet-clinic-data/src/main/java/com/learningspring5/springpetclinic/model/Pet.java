08f4a5e8f89783be64851f7491f82d00574d4a99
mehj123 <mehjabeen.begum@outlook.com>
2019-06-16 10:52:19 +0400

    Added JPA annotations for Owner Entity. Closes #37

--- spring-pet-clinic-data/src/main/java/com/learningspring5/springpetclinic/model/Pet.java
+++ spring-pet-clinic-data/src/main/java/com/learningspring5/springpetclinic/model/Pet.java
@@ -1,12 +1,24 @@
 package com.learningspring5.springpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name="pets")
 public class Pet extends BaseEntity{
 
+    @Column(name="name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name="type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name="owner_id")
     private Owner owner;
+
+    @Column(name="birth_date")
     private LocalDate birthDate;
 
     @Override
@@ -27,7 +39,7 @@ public class Pet extends BaseEntity{
         this.petType = petType;
     }
 
-    public Owner getOwner() {
+         public Owner getOwner() {
         return owner;
     }
 
--- spring-pet-clinic-data/src/main/java/com/learningspring5/springpetclinic/model/PetType.java
+++ spring-pet-clinic-data/src/main/java/com/learningspring5/springpetclinic/model/PetType.java
@@ -1,4 +1,24 @@
 package com.learningspring5.springpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="types")
 public class PetType extends BaseEntity {
+
+    @Column(name="name")
+    private String name;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
