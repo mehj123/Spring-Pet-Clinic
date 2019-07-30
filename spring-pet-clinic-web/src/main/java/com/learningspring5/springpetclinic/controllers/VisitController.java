6074eefbf286e7cf0a9e3010b6f4c18acabb40d7
mehj123 <mehjabeen.begum@outlook.com>
2019-07-30 15:40:59 +0400

    Added code to add or edit visits- Closes #58

---
+++ spring-pet-clinic-web/src/main/java/com/learningspring5/springpetclinic/controllers/VisitController.java
@@ -0,0 +1,58 @@
+package com.learningspring5.springpetclinic.controllers;
+
+import com.learningspring5.springpetclinic.model.Pet;
+import com.learningspring5.springpetclinic.model.Visit;
+import com.learningspring5.springpetclinic.services.PetService;
+import com.learningspring5.springpetclinic.services.VisitService;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Map;
+
+@Controller
+public class VisitController {
+
+    private final VisitService visitService;
+    private final PetService petService;
+
+    public VisitController(VisitService visitService, PetService petService) {
+        this.visitService = visitService;
+        this.petService = petService;
+    }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
+
+    @ModelAttribute("visit")
+    public Visit loadPetWithVisit(@PathVariable("petId") Long petId, Map<String, Object> model) {
+        Pet pet = petService.findById(petId);
+        Visit visit = new Visit();
+        visit.setPet(pet);
+        pet.getVisits().add(visit);
+        model.put("pet", pet);
+        return visit;
+    }
+
+    // Spring MVC calls method loadPetWithVisit(...) before initNewVisitForm is called
+    @GetMapping("/owners/*/pets/{petId}/visits/new")
+    public String initNewVisitForm(@PathVariable("petId") Long petId, Map<String, Object> model) {
+        return "pets/createOrUpdateVisitForm";
+    }
+
+    // Spring MVC calls method loadPetWithVisit(...) before processNewVisitForm is called
+    @PostMapping("/owners/{ownerId}/pets/{petId}/visits/new")
+    public String processNewVisitForm(@Valid Visit visit, BindingResult result) {
+        if (result.hasErrors()) {
+            return "pets/createOrUpdateVisitForm";
+        } else {
+            visitService.save(visit);
+            return "redirect:/owners/{ownerId}";
+        }
+    }
+}
